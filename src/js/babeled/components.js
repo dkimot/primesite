dba49a6fe3ecf235eaeb085d1a35a87e65c28b25
Davis Kimoto <davis.kimoto@gmail.com>
2017-02-27 17:18:58 -0700

    finishing store ajax

--- src/js/babeled/components.js
+++ src/js/babeled/components.js
@@ -1,6 +1,6 @@
-var item = '<p item="asdf" onclick="log()">item\'a\'</p><script>var log = function log() {  console.log(\'asdf\');}</script>';
+var product = '<div class="product">  <h3 class="product" id="product-name"></h3>  <img src="" alt="" id="product-image" />  <h4 class="product" id="product-slug"></h4>  <p class="product" id="product-description"></p>  <h2 class="product" id="product-price"></h2></div>';
 
 var components = {
-  item: item,
+  product: product,
   null: 'null'
 };
