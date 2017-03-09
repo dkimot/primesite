332456ff68bef7e8bcb4cc19bd38e81c9b610481
Davis Kimoto <davis.kimoto@gmail.com>
2017-03-09 15:19:45 -0700

    lots

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 const favicon    = require('serve-favicon');
 const path       = require('path');
 
+const PORT = process.env.PORT || 3030;
+
 // Create new server
 const server = express();
 
@@ -14,13 +16,16 @@ server.use(bodyParser.json());
 server.set('views', './views');
 server.set('view engine', 'pug');
 
-server.post('/contact/sendmail', (req, res) => {
-  // Send email
-});
-
 server.use('/build', express.static(path.join(__dirname, 'public', 'build')));
 server.use('/img', express.static(path.join(__dirname, 'public', 'img')));
 server.use('/fonts', express.static(path.join(__dirname, 'public', 'fonts')));
+
+server.post('/contact/sendmail', (req, res) => {
+  // Send email
+});
+server.get('/products', (req, res) => {
+  res.redirect('http://shop.primesystemsinc.com');
+});
 
 server.get('/*', (req, res, next) => {
   let url = req.url.slice(1);
@@ -54,6 +59,15 @@ server.use((err, req, res, next) => {
 })
 */
 
-server.listen(3030, () => {
-  console.log('Server listening on %s', 3030);
+server.listen(3030, (err) => {
+  if (err)
+    console.error(err)
+  else
+    console.log('Server listening on %s', 3030);
 });
+
+if (process.env.environment === 'PRODUCTION') {
+  process.on('uncaughtException', (err) => {
+    console.err(JSON.parse(JSON.stringify(err, ['stack', 'message', 'inner'], 2)));
+  });
+}
