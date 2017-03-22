a898effae21bb953cfa0c6bfc77cb763fefd8918
Davis Kimoto <davis.kimoto@gmail.com>
2017-03-22 13:53:47 -0600

    added content-type to allowed headers

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const server = express();
 const allowCrossDomain = function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,POST');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
 
   next();
 }
