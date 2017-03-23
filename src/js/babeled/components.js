a1af03225a15d17a9c4c3ecb2a2f10dc429028f1
Davis Kimoto <daviskimoto@Daviss-MacBook-Pro.local>
2017-03-23 14:58:12 -0600

    Initial branch commit

--- src/js/babeled/components.js
+++
@@ -1,6 +0,0 @@
-var product = '<div class="product">  <h3 class="product" id="product-name"></h3>  <img src="" alt="" id="product-image" />  <h4 class="product" id="product-slug"></h4>  <p class="product" id="product-description"></p>  <h2 class="product" id="product-price"></h2></div>';
-
-var components = {
-  product: product,
-  null: 'null'
-};
