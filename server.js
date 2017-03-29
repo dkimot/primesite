9f44ada19bcd07c115494afb06f40d62c13359dc
Davis Kimoto <davis.kimoto@gmail.com>
2017-03-29 11:32:31 -0600

    Client server and server seperation

--- server.js
+++ server.js
@@ -1,13 +1,1 @@
-const express    = require('express');
-const bodyParser = require('body-parser');
-const path       = require('path');
-
-const PORT = process.env.PORT || 3030;
-
-const site = express();
-
-site.use(express.static('client/dist'));
-
-site.listen(PORT, () => {
-  console.log('Site is listening on %s', PORT);
-});
+require('./client/server');
