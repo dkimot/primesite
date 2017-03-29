7a33969333375d54095c9ee44f3d5a84d5cb84e0
Davis Kimoto <davis.kimoto@gmail.com>
2017-03-28 22:38:07 -0600

    End of work day

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-const express = require('express');
+const express    = require('express');
+const bodyParser = require('body-parser');
+const path       = require('path');
+
+const PORT = process.env.PORT || 3030;
+
+const site = express();
+
+site.use(express.static('client/dist'));
+
+site.listen(PORT, () => {
+  console.log('Site is listening on %s', PORT);
+});
