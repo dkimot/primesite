df1593e6a9ec6991f3c4c4db2b40040f9df796bd
Davis Kimoto <davis.kimoto@gmail.com>
2017-03-30 15:21:22 -0600

    Added category based product table to catalog.js

--- client/lib/containers/Catalog.js
+++ client/lib/containers/Catalog.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { filterTable } from '../actions';
+import ProductTable from '../components/ProductTable';
+import { filterableTable } from '../styles/filterableTable.scss';
 
-const Catalog = (props) => (
-  <div>
-    <h3>Category: {props.params.category}</h3>
-    <h3>Slug: {props.params.slug}</h3>
-  </div>
-)
+const Catalog = (props) => {
+  if (props.params.category && !props.params.slug) {
+    return (
+      <div>
+        <h3>Category: {props.params.category}</h3>
+        <h3>Slug: {props.params.slug}</h3>
+        <div>
+          <ProductTable category={props.params.category} />
+        </div>
+      </div>
+    )
+  } else if (props.params.category && props.params.slug) {
+    return (
+      <div>
+        <h3>Category: {props.params.category}</h3>
+        <h3>Slug: {props.params.slug}</h3>
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <h3>Full Category</h3>
+      </div>
+    )
+  }
+}
 
 Catalog.propTypes = {
   params: React.PropTypes.object
