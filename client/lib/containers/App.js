23cc521c913a177d2dd068e0c06b9f7416508e4c
Davis Kimoto <davis.kimoto@gmail.com>
2017-03-30 14:57:00 -0600

    Added links to categories to footer

--- client/lib/containers/App.js
+++ client/lib/containers/App.js
@@ -4,11 +4,16 @@ import { footer } from '../styles/footer.scss';
 
 const App = ({ children }) =>
   <div>
-    <h1>Filter Table</h1>
+    <h1>Prime Systems Catalog</h1>
     { children }
     <footer className={footer}>
-      <Link to="/">Filterable Table</Link>
-      <Link to="/about">About</Link>
+      <Link to="/">Catalog</Link>
+      <Link to="/desktops">Desktops</Link>
+      <Link to="/laptops">Laptops</Link>
+      <Link to="/components">Components</Link>
+      <Link to="/accessories">Accessories</Link>
+      <Link to="/pre-owned">Pre-Owned</Link>
+      <Link to="/search">Search</Link>
     </footer>
   </div>;
 
