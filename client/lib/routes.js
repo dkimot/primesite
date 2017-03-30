9b2ce4539338efd82d42d00bc766a4ed19c12cae
Davis Kimoto <davis.kimoto@gmail.com>
2017-03-30 14:56:43 -0600

    Pushed dynamic routes to root

--- client/lib/routes.js
+++ client/lib/routes.js
@@ -4,12 +4,13 @@ import App from './containers/App';
 import FilterableTable from './containers/FilterableTable';
 import About from './components/About';
 import Catalog from './containers/Catalog';
+import CatalogSearch from './containers/CatalogSearch';
 
 export default (
   <Route path="/" component={App}>
-    <IndexRoute component={FilterableTable} />
-    <Route path="/about" component={About} />
-    <Route path="/products/:category" component={Catalog} />
-    <Route path="/products/:category/:slug" component={Catalog} />
+    <IndexRoute component={Catalog} />
+    <Route path="/search" component={CatalogSearch} />
+    <Route path="/:category" component={Catalog} />
+    <Route path="/:category/:slug" component={Catalog} />
   </Route>
 );
