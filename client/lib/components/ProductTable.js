c5615d3ada379f0ff4a914543091c480b60106ed
Davis Kimoto <davis.kimoto@gmail.com>
2017-03-30 15:20:58 -0600

    Mock data categories now applicable to app

--- client/lib/components/ProductTable.js
+++ client/lib/components/ProductTable.js
@@ -2,33 +2,45 @@ import React, { PropTypes } from 'react';
 import ProductRow from './ProductRow';
 
 const products = [
-  { category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football' },
-  { category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball' },
-  { category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball' },
-  { category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch' },
-  { category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5' },
-  { category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7' }
+  { category: 'Desktops', price: '$49.99', stocked: true, name: 'Football' },
+  { category: 'Laptops', price: '$9.99', stocked: true, name: 'Baseball' },
+  { category: 'Components', price: '$29.99', stocked: false, name: 'Basketball' },
+  { category: 'Pre-Owned', price: '$99.99', stocked: true, name: 'iPod Touch' },
+  { category: 'Accessories', price: '$399.99', stocked: false, name: 'iPhone 5' },
+  { category: 'Desktops', price: '$199.99', stocked: true, name: 'Nexus 7' }
 ];
 
-const ProductTable = ({ filter }) => {
+const ProductTable = ({ filter, category }) => {
   let rows = [];
+  if (category) {
+    products.forEach((p) => {
+      const pCategory = p.category.toLowerCase();
 
-  products.forEach((p) => {
-    const nameLC = p.name.toLowerCase();
-    const filterLC = filter.toLowerCase();
+      if (pCategory === category) {
+        rows.push(
+          <ProductRow key={p.name} data={p} />
+        );
+      }
+    });
+  } else {
+    products.forEach((p) => {
+      const nameLC = p.name.toLowerCase();
+      const filterLC = filter.toLowerCase();
 
-    if (nameLC.indexOf(filterLC) !== -1) {
-      rows.push(
-        <ProductRow key={p.name} data={p} />
-      );
-    }
-  });
+      if (nameLC.indexOf(filterLC) !== -1) {
+        rows.push(
+          <ProductRow key={p.name} data={p} />
+        );
+      }
+    });
+  }
 
   return <div> {rows} </div>;
 };
 
 ProductTable.propTypes = {
-  filter: PropTypes.string
+  filter: PropTypes.string,
+  category: PropTypes.string
 };
 
 export default ProductTable;
