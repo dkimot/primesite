94eebaf2d5a48204fa1877baee3066a1d383ccd1
Davis Kimoto <davis.kimoto@gmail.com>
2017-03-30 14:57:14 -0600

    Adjusted Catalog to show category and slug

--- client/lib/containers/Catalog.js
+++ client/lib/containers/Catalog.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Catalog = (props) => (
   <div>
-    <h3>ID: {props.params.category}</h3>
-    <h3>ID: {props.params.slug}</h3>
+    <h3>Category: {props.params.category}</h3>
+    <h3>Slug: {props.params.slug}</h3>
   </div>
 )
 
